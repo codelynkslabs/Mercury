c191b6973c820416e02d3b71b3934153c83dffa9
Satheesh S <108679701+sath-eesh@users.noreply.github.com>
2024-02-23 18:18:02 +0530

    * Add podspec file (#2)
    * Update color conversion function

--- Sources/Mercury/Colors/MercuryColors.swift
+++ Sources/Mercury/Colors/MercuryColors.swift
@@ -12,52 +12,41 @@ import UIKit.UIColor
 // MARK: - Colors
 
 public struct MercuryColor {
-  public let rgbaValue: UInt32
+  public let hexValue: Int
   public var color: UIColor { return UIColor(named: self) }
 
   public enum Base {
-    public static let xfdb426 = MercuryColor(rgbaValue: 0xfdb426)
-    public static let xffffff = MercuryColor(rgbaValue: 0xffffff)
-    public static let xfcfcfc = MercuryColor(rgbaValue: 0xfcfcfc)
-    public static let x25292d = MercuryColor(rgbaValue: 0x25292d)
+    public static let xfdb426 = MercuryColor(hexValue: 0xfdb426)
+    public static let xffffff = MercuryColor(hexValue: 0xffffff)
+    public static let xfcfcfc = MercuryColor(hexValue: 0xfcfcfc)
+    public static let x25292d = MercuryColor(hexValue: 0x25292d)
   }
 }
 
 // MARK: - Implementation Details
 
 internal extension UIColor {
-  convenience init(rgbaValue: UInt32) {
-    let components = RGBAComponents(rgbaValue: rgbaValue).normalized
-    self.init(red: components[0], green: components[1], blue: components[2], alpha: components[3])
-  }
-}
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
 
-private struct RGBAComponents {
-  let rgbaValue: UInt32
-
-  private var shifts: [UInt32] {
-    [
-      rgbaValue >> 24, // red
-      rgbaValue >> 16, // green
-      rgbaValue >> 8,  // blue
-      rgbaValue        // alpha
-    ]
-  }
-
-  private var components: [CGFloat] {
-    shifts.map {
-      CGFloat($0 & 0xff)
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
-  }
 
-  var normalized: [CGFloat] {
-    components.map { $0 / 255.0 }
-  }
+    convenience init(hexValue: Int) {
+        self.init(
+            red: (hexValue >> 16) & 0xFF,
+            green: (hexValue >> 8) & 0xFF,
+            blue: hexValue & 0xFF
+        )
+    }
 }
 
 public extension UIColor {
   convenience init(named color: MercuryColor) {
-    self.init(rgbaValue: color.rgbaValue)
+    self.init(hexValue: color.hexValue)
   }
 }
 
