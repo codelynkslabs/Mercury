851d13803fb279cd10e094af33e3b7fe3583fab2
Satheesh S <108679701+sath-eesh@users.noreply.github.com>
2024-02-23 11:55:58 +0530

    Update Package.swift
    Co-authored-by: Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Mercury",
             targets: ["Mercury"]),
